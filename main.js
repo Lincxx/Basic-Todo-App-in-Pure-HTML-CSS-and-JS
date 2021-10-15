6253d2b82718adca5f9ce1e578420e513b6ef392
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-10-15 13:22:51 -0400

    Added in a move function

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ const get = id => document.getElementById(id);
 const newText       = get('newText');
 const newBtn        = get('newBtn');
 const pendingList   = get('pendingList');
-const completedList = get('newTcompletedListext');
+const completedList = get('completedList');
 
 newBtn.addEventListener('click', () => {
     if (newText.value !== "") {
          const newTodo = document.createElement('li');
          newTodo.innerText = newText.value;
          pendingList.appendChild(newTodo);
-         newTodo = ''
+         newTodo.value = ''
     } else {
         alert("Please provide a task to add");
     }
-    
 });
+
+
+const move = (element, destination) => {
+    if (element.localName === 'li') {
+        destination.appendChild(element);
+    }  
+};
+
+pendingList.addEventListener('click', event => {
+   move(event.target, completedList) 
+})
+
+completedList.addEventListener('click', event => {
+    move(event.target, pendingList) 
+ })
